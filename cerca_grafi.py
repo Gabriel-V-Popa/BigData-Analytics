33aff6b86474b457bf2f1fd85a9cd40765d4d573
enzoc2000 <s1121673@studenti.univpm.it>
2026-06-04 17:31:16 +0200

    reinserimento matrici corrette con esperimenti

--- cerca_grafi.py
+++ cerca_grafi.py
@@ -39,7 +39,7 @@ if __name__ == "__main__":
     # ==================================================
     # CAMBIA QUESTO VALORE PER CERCARE ALTRE ANOMALIE
     # ==================================================
-    sub_da_cercare = "Sub2" 
+    sub_da_cercare = "Sub63" 
     
     print(f"🔍 Ricerca dei grafi per l'anomalia: {sub_da_cercare}...\n")
     cerca_grafi(PERCORSO_CSV, sub_da_cercare)
